66905b92961cb77bf65cfc4388520589fce75296
Georgia <georgialhomer@gmail.com>
2018-12-02 21:59:02 +0000

    Changed photos
    Changed to individual candle photos

---
+++ js/luma.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    $('.xmasinfo').hide();
+
+  $('#xmas').hover(function() {
+ $('.xmasinfo').show()
+  },function() {
+      $('.xmasinfo').hide()
+  });
+});
+$(document).ready(function(){
+    $('.chocinfo').hide();
+
+  $('#choc').hover(function() {
+ $('.chocinfo').show()
+  },function() {
+      $('.chocinfo').hide()
+  });
+});
+$(document).ready(function(){
+    $('.violetinfo').hide();
+
+  $('#violet').hover(function() {
+ $('.violetinfo').show()
+  },function() {
+      $('.violetinfo').hide()
+  });
+});
+$(document).ready(function(){
+    $('.loveinfo').hide();
+
+  $('#love').hover(function() {
+ $('.loveinfo').show()
+  },function() {
+      $('.loveinfo').hide()
+  });
+});
+$(document).ready(function(){
+    $('.muffininfo').hide();
+
+  $('#muffin').hover(function() {
+ $('.muffininfo').show()
+  },function() {
+      $('.muffininfo').hide()
+  });
+});
+$(document).ready(function(){
+    $('.vanillainfo').hide();
+
+  $('#vanilla').hover(function() {
+ $('.vanillainfo').show()
+  },function() {
+      $('.vanillainfo').hide()
+  });
+});
